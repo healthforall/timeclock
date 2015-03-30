ca67ef72cae08c4504a8470a495c36c45d82f6ab
Zach Brown <brownzach125@gmail.com>
2015-03-30 15:43:41 -0600

    Made the correct clockin button appear

--- app/models/in_and_out.rb
+++ app/models/in_and_out.rb
@@ -2,5 +2,5 @@ class InAndOut < ActiveRecord::Base
   belongs_to :day
   scope      :find_dangling_in , lambda{
                InAndOut.where(:out => nil)
-                               }
+   }
 end
