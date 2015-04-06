81e0a22dcb04e92a3e08529bbe1aca19309feaec
eakellogg <eakellogg@tamu.edu>
2015-04-05 21:14:32 -0500

    added rspec tests for models (fixed pay period factory)

--- spec/factories/payperiod.rb
+++ spec/factories/payperiod.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 # creates a pay period that starts up to 8 days before today and ends up to 8 days after today
 
   factory :payperiod do |f|
-    f.start_date {Faker::Date.backward(8)}
+    f.start_date {Faker::Date.between(8.days.ago, 1.days.ago)}
     f.end_date {Faker::Date.forward(8)}
     #start_date do
      # DateTime.parse("2015-3-1 10:00:00")
