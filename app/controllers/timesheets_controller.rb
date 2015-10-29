901468b7b183c02f10fdc34f6b5b30cd064f6599
xiangpeng wang <wangxiangpeng@dhcp-10-201-78-36.tamulink.tamu.edu>
2015-10-28 22:25:12 -0500

    approve

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -19,7 +19,7 @@ class TimesheetsController < ApplicationController
     @name = @employee.name
     @uid  = @employee.uid
     @total_hours = @timesheet.totalHours
-    @approved = @timesheet.approved.to_s
+    @approved = @timesheet.approved
     respond_to do |format|
       format.html
       format.xls { headers["Content-Disposition"] = "attachment; filename=\"timesheet_" + @employee.name + "_" + @payperiod.file_print + ".xls\"" }
