35b6c2845bf47e3bdf628ea82829e3955b942132
xiangpeng wang <wangxiangpeng@dhcp-10-202-143-16.tamulink.tamu.edu>
2015-10-29 15:05:12 -0500

    add more admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,24 @@ payperiod = Payperiod.create!(payperiod)
 
 team = [   {:name =>'Zach'    , :email => "brownzach125@gmail.com" } ,    {:name =>'Elizabeth' , :email => "bookworm920@gmail.com"},
            {:name =>'Jon'     , :email => "jtgoen@gmail.com"}        ,    {:name =>'Michael'   , :email => "md.stephens9@gmail.com"},
-           {:name =>'Garret'  , :email => "swamp.fox076@gmail.com"}  ,    {:name =>'Steven'    , :email => "redynsnevets@gmail.com", :admin => true},
+           {:name =>'Garret'  , :email => "swamp.fox076@gmail.com"}  ,    {:name =>'Steven'    , :email => "redynsnevets@gmail.com"},
 		       {:name =>'Employee', :email => "employee20156@gmail.com"}  ,   {:name =>'Xiangpeng' , :email => 'wxp2002@gmail.com'} ,
            {:name =>'JingchengHuang' , :email => 'huang155439@gmail.com'},{:name =>'wutongzhang' , :email => 'wutongzhang@tamu.edu'} ,
             {:name => 'Tianshu', :email => "cts2003258@gmail.com"},       {:name => 'mona', :email => "ohnarya@gmail.com"}]
 
-admin = { :name =>'admin' , :email => 'wxp2002@tamu.edu' , :admin => true}
-admin = Employee.create!(admin)
-
-supervisor = { :name =>'Im a supervisor' , :email => 'suadhealth@gmail.com' }
-Employee.create!(supervisor)
-
-team.each do |mate|
-  admin.employees.create!( mate  )
+admin =[ { :name =>'admin' , :email => 'wxp2002@tamu.edu' , :admin => true},{ :name =>'tianshu' , :email => 'chutianshu@tamu.edu' , :admin => true}]
+adminList = []
+admin.each do |supervisor|
+  adminList << Employee.create!(supervisor)
 end
 
+#supervisor = { :name =>'Im a supervisor' , :email => 'suadhealth@gmail.com' }
+#Employee.create!(supervisor)
+adminList.each do |administrator|
+  team.each do |mate|
+    administrator.employees.create!( mate  )
+  end
+end
 
 
 
