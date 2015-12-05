ab1b80daf3a64dc467814349e640f457ba15a628
ohnary <ohnarya@tamu.edu>
2015-12-04 16:06:19 -0800

    add TDD on vacation_spec

--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require 'spec_helper'
+require 'factory_girl_rails'
 
 RSpec.describe Vacation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    before do
+        @vacation = FactoryGirl.create(:vacation, :name =>'Sick Leave', :hour=>8)   
+    end	
+  it "has a valid vacation request" do 
+    @vacation=FactoryGirl.create(:vacation)
+    expect(@vacation).to be_valid
+    expect(@vacation.name).to  be_a_kind_of(String)
+    expect(@vacation.hours).to be_a_kind_of(String)
+    expect(@vacation.type).to  be_a_kind_of(String)
+  end
 end
