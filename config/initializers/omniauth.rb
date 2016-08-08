33d7b331ccdd31c6fed5e04a60f1ff2bd9b8c969
Zach Brown <brownzach125@gmail.com>
2016-08-07 21:16:55 -0500

    OmniAuth now gets Client_ID and Client_SECRET from ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '583710601190-1encd5u3fsunkm3f32ul2nksqhbsdcs9.apps.googleusercontent.com', '2jHT1qF3S_Hg1Ys9ERd4LK2_',
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
            {
                scope: 'email',
                access_type: 'offline'
