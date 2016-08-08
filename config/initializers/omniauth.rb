51d53c3700c1c48795f91152dfeb0c787d2c377a
Zach Brown <brownzach125@gmail.com>
2016-08-07 21:36:53 -0500

    I made a mistake with a carrige return

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,5 +6,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            :scope=> 'email',
            :info_fields=> 'name,email'
-
 end
